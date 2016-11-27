b3a341c05d839fec2e46e1ad5305803b10d8e164
JorgeOrtizG <iepvjortiz@gmail.com>
2016-11-27 14:59:56 -0500

    Update Nodo.java

--- src/basedatos/Nodo.java
+++ src/basedatos/Nodo.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 
 import com.mysql.fabric.xmlrpc.base.Array;
 
+// Clase  que guarda la informacion de un nodo
 public class Nodo {
 	String valor;
 	int nodoId;
@@ -17,6 +18,8 @@ public class Nodo {
 		listaNodoHijos=new ArrayList<>();
 		listaNodoPadres=new ArrayList<>();
 	}
+	
+	//Sobreescrito para devolver la información del nodo en formato JSON
 	@Override
 	public String toString() {
 		return "{'Id':'" + nodoId +"','valor':'"+valor+"','condicion':'"+condicion+"','nivel':'"+nivel+"',"
@@ -24,7 +27,7 @@ public class Nodo {
 				+ listaNodoHijos +",'listaNodoPadres':"+listaNodoPadres+ "}";
 	}
 	
-	
+	//Método que permite asignar niveles de profundidad a los nodos del listado de nodos padres
 	public void estabilizarNivelPadres(int base){
 		for (Nodo nodo : listaNodoPadres) {
 			nodo.nivel=nodo.nivel+base;
